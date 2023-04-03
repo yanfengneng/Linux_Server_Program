8ab152537dc33a446a72045843545836aafefbab
yanfengneng <2412553598@qq.com>
2023-04-03 20:25:01 +0800

    更新注解

--- chapter5/5_3.cpp
+++ chapter5/5_3.cpp
@@ -34,14 +34,24 @@ int main(int argc,char* argv[])
     /*创建一个 IPv4 socket 地址*/
     struct sockaddr_in address;
     bzero(&address, sizeof(address));
+    // 使用 IPv4
     address.sin_family = AF_INET;
+    // 将字符串 ip 地址转换为网络字节序正数表示的 ip 地址，并把转换结果存储在 address.sin_addr 指向的内存中。
     inet_pton(AF_INET, ip, &address.sin_addr);
+    // 设置端口号
     address.sin_port = htons(port);
 
+    /*  
+        命名 socket：将 address 所指的 socket 地址分配给为命名的 sock 文件描述符，使用sizeof 来指出该地址的长度
+        bind 成功返回 0，失败返回 -1 并设置 errno。
+    */
     int ret = bind(sock, (struct sockaddr*)&address, sizeof(address));
+    // 失败就直接断言退出程序了
     assert(ret != -1);
 
+    /*监听 socket：监听 sock，使用 backlog 来提示内核监听队列的最大长度*/
     ret = listen(sock,backlog);
+    // 失败就直接断言退出程序了
     assert(ret != -1);
 
     /*循环等待连接，直到有 SIGTERM 信号将它中断*/
