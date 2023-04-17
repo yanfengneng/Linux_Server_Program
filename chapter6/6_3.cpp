f11af93c812cd235cc15ef6dce7e463f8b3b20ca
yanfengneng <2412553598@qq.com>
2023-04-17 14:52:00 +0800

    用 sendfile 函数传输文件

---
+++ chapter6/6_3.cpp
@@ -0,0 +1,63 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
+
+int main( int argc, char* argv[] )
+{
+    if( argc <= 3 )
+    {
+        printf( "usage: %s ip_address port_number filename\n", basename( argv[0] ) );
+        return 1;
+    }
+    const char* ip = argv[1];
+    int port = atoi( argv[2] );
+    // 将目标文件作为程序的第三个参数传入
+    const char* file_name = argv[3];
+
+    int filefd = open( file_name, O_RDONLY );
+    assert( filefd > 0 );
+    struct stat stat_buf;
+    fstat( filefd, &stat_buf );
+
+    struct sockaddr_in address;
+    bzero( &address, sizeof( address ) );
+    address.sin_family = AF_INET;
+    inet_pton( AF_INET, ip, &address.sin_addr );
+    address.sin_port = htons( port );
+
+    int sock = socket( PF_INET, SOCK_STREAM, 0 );
+    assert( sock >= 0 );
+
+    int ret = bind( sock, ( struct sockaddr* )&address, sizeof( address ) );
+    assert( ret != -1 );
+
+    ret = listen( sock, 5 );
+    assert( ret != -1 );
+
+    struct sockaddr_in client;
+    socklen_t client_addrlength = sizeof( client );
+    int connfd = accept( sock, ( struct sockaddr* )&client, &client_addrlength );
+    if ( connfd < 0 )
+    {
+        printf( "errno is: %d\n", errno );
+    }
+    else
+    {
+        // 用来将 filefd 中的文件内容写入到 connfd 里面，使用 filefd 中默认的起始位置，并指定这两个文件直接传递的字节数。
+        sendfile( connfd, filefd, NULL, stat_buf.st_size );
+        close( connfd );
+    }
+    // 关闭 socket 
+    close( sock );
+    return 0;
+}
