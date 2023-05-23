0ce91c65ebe6cc220f55091a66bc1f997166b96b
yanfengneng <2412553598@qq.com>
2023-05-23 21:47:23 +0800

    HTTP 请求的读取和分析

--- chapter08/8_3.cpp
+++ chapter08/8_3.cpp
@@ -27,8 +27,8 @@ static const char* szret[] = { "I get a correct result\n", "Something wrong\n" }
 LINE_STATUS parse_line( char* buffer, int& checked_index, int& read_index )
 {
     char temp;
-    /* checked_index 指向 buffer（应用程序的读缓冲区）中当前正在分析的字节，read_index 指向 buffer 中客户数据的尾部的下一字节，
-    buffer 中第 0~checked_index 字节都已分析完毕，第 checked_index~(read_index-1) 字节由下面的循环挨个分析 */
+    /* checked_index 指向 buffer（应用程序的读缓冲区）中当前正在分析的字节，read_index 指向 buffer 中客户数据的尾后的下一字节,也就是说 read_index 是不指向真正的字节的，
+    buffer 中第 0~checked_index-1 字节都已分析完毕，第 checked_index~(read_index-1) 字节由下面的循环挨个分析 */
     for ( ; checked_index < read_index; ++checked_index )
     {
         /* 获得当前要分析的字节 */
@@ -44,6 +44,7 @@ LINE_STATUS parse_line( char* buffer, int& checked_index, int& read_index )
             /* 如果下一个字符是"\n"，则说明我们成功读取到一个完整的行 */
             else if ( buffer[ checked_index + 1 ] == '\n' )
             {
+                // 将当前位置和下一个位置都设置为字符串结束符
                 buffer[ checked_index++ ] = '\0';
                 buffer[ checked_index++ ] = '\0';
                 return LINE_OK;
@@ -54,6 +55,7 @@ LINE_STATUS parse_line( char* buffer, int& checked_index, int& read_index )
         /* 如果当前的字节是"\n"，即换行符，则也说明可能读取到一个完整的行 */
         else if( temp == '\n' )
         {
+            // 由[回车符、换行符]组成表示读取到一个完全的行了。然后将当前位置和上一个位置都设置为字符串结束符。
             if( ( checked_index > 1 ) &&  buffer[ checked_index - 1 ] == '\r' )
             {
                 buffer[ checked_index-1 ] = '\0';
@@ -70,53 +72,65 @@ LINE_STATUS parse_line( char* buffer, int& checked_index, int& read_index )
 /* 分析请求行 */
 HTTP_CODE parse_requestline( char* szTemp, CHECK_STATE& checkstate )
 {
+    // 在 szURL 中匹配空白字符或者"\t"字符，若未找到字符则返回 NULL。
     char* szURL = strpbrk( szTemp, " \t" );
-    /* 如果请求行中没有空白祖父或者"\t"字符，则 HTTP 请求必有问题 */
+    /* 如果请求行中没有空白字符或者"\t"字符，则 HTTP 请求必有问题 */
     if ( ! szURL ){
         return BAD_REQUEST;
     }
+
+    /* 找到空白字符或者"\t"字符了，则将其设置为字符串结束符，并将 szURL 移动到下一个位置。 */
     *szURL++ = '\0';
 
     char* szMethod = szTemp;
     /* 仅支持 GET 方法 */
-    if ( strcasecmp( szMethod, "GET" ) == 0 )
+    if ( strcasecmp( szMethod, "GET" ) == 0 )// 比较 szMethod 与 "GET" 字符串，比较时会自动忽略大小写。两个字符串相等则返回 0，则就返回非 0 值。
     {
         printf( "The request method is GET\n" );
     }
+    /* 不支持 GET 方法，则该 HTTP 请求有问题 */
     else
     {
         return BAD_REQUEST;
     }
 
+    // 检索字符串 szURL 中第一个不在字符串 " \t" 中出现的字符下标。也就是跳过 szURL 中连续的空白字符和回车符
     szURL += strspn( szURL, " \t" );
     char* szVersion = strpbrk( szURL, " \t" );
+    /* 如果请求行中没有空白字符或者"\t"字符，则 HTTP 请求必有问题 */
     if ( ! szVersion )
     {
         return BAD_REQUEST;
     }
+    /* 找到空白字符或者"\t"字符了，则将其设置为字符串结束符，并将 szVersion 移动到下一个位置。 */
     *szVersion++ = '\0';
+    // 跳过连续的空白字符或者"\t"字符了
     szVersion += strspn( szVersion, " \t" );
     /* 仅支持 HTTP/1.1 */
-    if ( strcasecmp( szVersion, "HTTP/1.1" ) != 0 )
+    if ( strcasecmp( szVersion, "HTTP/1.1" ) != 0 ) // 这两个字符串相同就返回0，不相同返回非 0 值。
     {
+        // 不相同返回 HTTP 请求有问题
         return BAD_REQUEST;
     }
 
     /* 检查 URL 是否合法 */ 
-    if ( strncasecmp( szURL, "http://", 7 ) == 0 )
+    if ( strncasecmp( szURL, "http://", 7 ) == 0 )// 合法
     {
-        szURL += 7;
-        szURL = strchr( szURL, '/' );
+        szURL += 7;// 右移7位
+        szURL = strchr( szURL, '/' );// szURL 移动到'/'在 szURL 第一次出现的位置
     }
 
+    // szURL 为空或者第一个字符不为'/'，则该 HTTP 请求有问题
     if ( ! szURL || szURL[ 0 ] != '/' )
     {
         return BAD_REQUEST;
     }
 
+    // 最终得到 HTTP 请求的 URL 了
     printf( "The request URL is: %s\n", szURL );
     /* HTTP 请求行处理完毕，状态转移到头部字段的分析 */
     checkstate = CHECK_STATE_HEADER;
+    // 返回请求不完整，需要继续读取客户数据
     return NO_REQUEST;
 }
 
@@ -128,6 +142,7 @@ HTTP_CODE parse_headers( char* szTemp )
     {
         return GET_REQUEST;
     }
+    /* 解析到"Host:"字段，然后将 szTemp 右移 5 位，跳过连续的空格和'\t'字符，打印出主机地址。 */
     else if ( strncasecmp( szTemp, "Host:", 5 ) == 0 )
     {
         szTemp += 5;
@@ -140,6 +155,7 @@ HTTP_CODE parse_headers( char* szTemp )
         printf( "I can not handle this header\n" );
     }
 
+    // 返回请求不完整字段
     return NO_REQUEST;
 }
 
@@ -163,6 +179,7 @@ HTTP_CODE parse_content( char* buffer, int& checked_index, CHECK_STATE& checksta
             /* 第一个状态：分析请求行 */
             case CHECK_STATE_REQUESTLINE:
             {
+                // 分析请求行
                 retcode = parse_requestline( szTemp, checkstate );
                 if ( retcode == BAD_REQUEST )
                 {
@@ -173,6 +190,7 @@ HTTP_CODE parse_content( char* buffer, int& checked_index, CHECK_STATE& checksta
             /* 第二个状态：分析头部字段 */
             case CHECK_STATE_HEADER:
             {
+                // 分析头部字段
                 retcode = parse_headers( szTemp );
                 if ( retcode == BAD_REQUEST )
                 {
@@ -186,6 +204,7 @@ HTTP_CODE parse_content( char* buffer, int& checked_index, CHECK_STATE& checksta
             }
             default:
             {
+                // 返回服务器内部错误
                 return INTERNAL_ERROR;
             }
         }
@@ -215,17 +234,21 @@ int main( int argc, char* argv[] )
     inet_pton( AF_INET, ip, &address.sin_addr );
     address.sin_port = htons( port );
     
+    // 创建 socket
     int listenfd = socket( PF_INET, SOCK_STREAM, 0 );
     assert( listenfd >= 0 );
     
+    // 绑定 socket 地址
     int ret = bind( listenfd, ( struct sockaddr* )&address, sizeof( address ) );
     assert( ret != -1 );
     
+    // 开始监听 socket
     ret = listen( listenfd, 5 );
     assert( ret != -1 );
     
     struct sockaddr_in client_address;
     socklen_t client_addrlength = sizeof( client_address );
+    // 从等待队列中选择一个客户端 socket 进行通信
     int fd = accept( listenfd, ( struct sockaddr* )&client_address, &client_addrlength );
     if( fd < 0 )
     {
@@ -245,12 +268,14 @@ int main( int argc, char* argv[] )
         /* 循环读取客户数据并分析之 */
         while( 1 )
         {
+            // 接收客户端的数据
             data_read = recv( fd, buffer + read_index, BUFFER_SIZE - read_index, 0 );
             if ( data_read == -1 )
             {
                 printf( "reading failed\n" );
                 break;
             }
+            // 没有数据表示客户端已经关闭连接
             else if ( data_read == 0 )
             {
                 printf( "remote client has closed the connection\n" );
@@ -268,12 +293,14 @@ int main( int argc, char* argv[] )
             // 得到一个完整的、正确的 HTTP 请求
             else if( result == GET_REQUEST )
             {
+                // 给客户端发送解析成功的信息
                 send( fd, szret[0], strlen( szret[0] ), 0 );
                 break;
             }
             // 其他情况表示错误发生
             else
             {
+                // 给客户端发送解析失败的信息
                 send( fd, szret[1], strlen( szret[1] ), 0 );
                 break;
             }
